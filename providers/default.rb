141a3ccfe1fdb9ab877f7f083264280d40abab41
Alexander Zubkov <info@zubkov.info>
2016-04-27 21:31:53 +0300

    Add support for start action

--- providers/default.rb
+++ providers/default.rb
@@ -25,6 +25,22 @@ action :create do
   end
 end
 
+action :start do
+  name = @new_resource.name
+  if jail_running?
+    Chef::Log.info "#{@new_resource} already running - nothing to do."
+  elsif !jail_exists?
+    raise Chef::Exceptions::Exec, "#{@new_resource} is not created, cannot start"
+  else
+    cmdStr = "ezjail-admin start #{name}"
+    execute cmdStr do
+      Chef::Log.debug "ezjail-admin_start: #{cmdStr}"
+      environment "PATH" => "/bin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin"
+      new_resource.updated_by_last_action(true)
+    end
+  end
+end
+
 def jail_exists?(name)
   cmdStr = "ezjail-admin list | grep '^#{name}\\b'"
   cmd = Mixlib::ShellOut.new(cmdStr)
@@ -38,4 +54,8 @@ def jail_exists?(name)
   rescue
     false
   end
+end
+
+def jail_running?
+  ::File.exist?("/var/run/jail_#{@new_resource.name}.id")
 end
