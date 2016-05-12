425b039ede6d94e79377452779246d56d00581f3
Alexander Zubkov <info@zubkov.info>
2016-05-12 11:51:42 +0300

    Version 0.2

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'brkane@eatliftcode.com'
 license          'Simplified BSD'
 description      'Installs/Configures jails in FreBSD'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 depends "line"
